e3366963eb5599bcd4d6c2303d86b11ab00a31cd
leandro <0xleanlp@gmail.com>
2022-12-08 12:39:31 -0300

    add conv bytes32toString proposals

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  INFURA_API_KEY: "0c3f076764d54001a4792cede6fb330a",
+  
+  ballotContract: "0x06157a790bc1b3f4f337859686c32f0123084331",
 };
 
 /*
